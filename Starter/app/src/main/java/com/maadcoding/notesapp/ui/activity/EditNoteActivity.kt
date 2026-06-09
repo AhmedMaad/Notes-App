d4cccea51cedd5feb3c8e59b18ffbf9b8742faa8
Ahmed Atef <atef.ahmed1341@gmail.com>
2026-06-09 19:19:09 +0300

    add starter code

---
+++ Starter/app/src/main/java/com/maadcoding/notesapp/ui/activity/EditNoteActivity.kt
@@ -0,0 +1,94 @@
+package com.maadcoding.notesapp.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.LocalActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.maadcoding.notesapp.ui.theme.NotesAppTheme
+
+class EditNoteActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            NotesAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    EditNoteScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EditNoteScreen(
+    modifier: Modifier = Modifier,
+) {
+
+    val activity = LocalActivity.current
+    val context = LocalContext.current
+    val noteFieldState = rememberTextFieldState()
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 32.dp)
+    ) {
+
+
+        OutlinedTextField(
+            state = noteFieldState,
+            label = { Text("Note details") },
+            modifier = Modifier
+                .padding(top = 64.dp)
+                .fillMaxWidth()
+        )
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(top = 32.dp)
+        ) {
+            OutlinedButton(
+                onClick = { },
+                modifier = modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Update")
+            }
+            OutlinedButton(
+                onClick = { },
+                modifier = modifier
+                    .weight(1f)
+                    .padding(start = 16.dp)
+            ) {
+                Text(text = "Delete")
+            }
+        }
+    }
+}
+
+@Preview(showSystemUi = true, device = "spec:width=411dp,height=891dp")
+@Composable
+private fun EditNoteScreenPreview() {
+    EditNoteScreen()
+}
